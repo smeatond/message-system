727240e0c7acb5b7ba91bb4beec1ad8c634059bd
Deanne Smeaton <deanne.smeaton@gmail.com>
2024-02-12 03:47:22 +1300

    create persistant data

--- MessageWeb/Hubs/MessageHub.cs
+++ MessageWeb/Hubs/MessageHub.cs
@@ -1,11 +1,19 @@
+using MessageWeb.Models;
+using MessageWeb.Services;
 using Microsoft.AspNetCore.SignalR;
 
 namespace MessageWeb.Hubs
 {
     public class MessageHub : Hub
     {
+         private readonly MessageService _messageService;
+
+    public MessageHub(MessageService messageService) =>
+        _messageService = messageService;
         public async Task SendMessage(string user, string message)
         {
+            var newMessage = new Message{MessageText = message, Username = user, DateTime = new DateTime()};
+            await _messageService.CreateAsync(newMessage);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
